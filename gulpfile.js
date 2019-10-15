1ce8eb5aa5b199d926baecc87fb891ff3285985e
Areta <aretakashta@arkit.ch>
2019-10-15 16:34:40 +0200

    completed heading and first block

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,3 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
-
-gulp.task('default', function(){
-    console.log('My first gulp task');
-});
-gulp.task('html', function(){
-    console.log('Some html here');
-});
-gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-gulp.task('watch', function(){
-    watch('./app/index.html', function(){
-        gulp.start('html');
-    })
-    watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles')
-    })
-})
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch')
+// require('./gulp/tasks/styles')
