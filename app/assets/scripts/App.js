c004dec0839a9c69d907f17f750b5a03984498c4
Areta <aretakashta@arkit.ch>
2019-10-29 12:13:29 +0100

    sticky header on master branch. problem with merge

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu(); //object that uses MobileMenu as a blueprint
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
